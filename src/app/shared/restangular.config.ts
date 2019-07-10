cfff81b199eb1d4684c2c7c5422b7f1e8261984d
soutionscience <marshnjagi@yahoo.com>
2019-07-10 12:31:25 +0300

    added loading animation

--- src/app/shared/restangular.config.ts
+++ src/app/shared/restangular.config.ts
@@ -1,6 +1,10 @@
+// const FFCApi ='https://ffc-server.herokuapp.com/api/';
+// const FFCLocal ='http://localhost:3000/api/'
+// const playerLocal = 'http://localhost:3000/api/players'
+
 const FFCApi ='https://ffc-server.herokuapp.com/api/';
-const FFCLocal ='http://localhost:3000/api/'
-const playerLocal = 'http://localhost:3000/api/players'
+const FFCLocal ='http://localhost:3002/api/'
+const playerLocal = 'http://localhost:3002/api/players'
 
 export function restangularConfigFactory(RestangularProvider){
     RestangularProvider.setBaseUrl(FFCLocal)
