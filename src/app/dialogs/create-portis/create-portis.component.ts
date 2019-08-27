41b193053a2a81ec672333a34f2cf178d4ff2cec
soutionscience <marshnjagi@yahoo.com>
2019-08-27 14:07:11 +0300

    checked if user is registed

--- src/app/dialogs/create-portis/create-portis.component.ts
+++ src/app/dialogs/create-portis/create-portis.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Web3Service } from '../../util/web3.service';
+import { AuthService } from '../../util/auth.service';
+import { ApiServiceService } from '../../util/api-service.service';
 
 @Component({
   selector: 'app-create-portis',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-portis.component.scss']
 })
 export class CreatePortisComponent implements OnInit {
+  emailValue: String;
 
-  constructor() { }
+  constructor(private web: Web3Service, private auth: AuthService, 
+    private api: ApiServiceService) { }
 
   ngOnInit() {
+  }
+
+
+  loginWithPortis(){
+    console.log('clicked bitch');
+    this.web.initPortisAndGetAccount()
+    .subscribe(resp=>{
+     if(resp.email){
+       this.emailValue = resp.email
+      this.checkBrowser(resp.address)
+
+     }else{
+       this.emailValue = '';
+       this.checkBrowser(resp)
+     }
+
+    })
+  }
+
+  checkIfRegisterd(id){
+  this.api.getSpecificResource('users', id)
+  .subscribe(resp=>{
+    console.log('user is registered')
+  }, error=>{
+    console.log('user does not exit')
+  })
+    
+    
+  }
+  checkBrowser(address){
+    if(this.auth.getUserAdress()){
+      console.log('user address in browser')
+    }else{
+      console.log('no user address in browser');
+      this.checkIfRegisterd(address)
+    }
+
   }
 
 }
--- src/app/util/web3.service.ts
+++ src/app/util/web3.service.ts
@@ -5,10 +5,15 @@ import { Factory, League } from '../shared/address';
 import { ObserversModule } from '@angular/cdk/observers';
 import { TokenService } from './token.service';
 import { AuthService } from './auth.service';
+import Portis from '@portis/web3';
+
 
 declare let require: any;
 const Web3 = require('web3');
 // const web3= Web3();
+
+const portis = new Portis('8b3ef407-9c59-4306-a90e-34f22c89f485', 'mainnet', { scope: ['email'], registerPageByDefault: true  });
+
 
 
 declare let window: any;
@@ -147,6 +152,29 @@ if(myaccount){
         observer.complete()
       }
 
+    })
+
+  }
+
+  //portis 
+
+  initPortisAndGetAccount(): Observable<any>{
+    console.log('are we getting this')
+    this.web3 = new Web3(portis.provider);
+    return Observable.create(observer=>{
+      portis.onLogin((walletAddress, email, reputation)=>{
+        let obj = {
+          account: walletAddress,
+          email: email
+        }
+        observer.next(obj)
+        observer.complete()
+      })
+      this.getCoinBase().subscribe(resp=>{
+        this.account = resp
+        observer.next(resp);
+        observer.complete()
+      })
     })
 
   }
